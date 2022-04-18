42f4cc430d85b351910c1c78bad2226e1c306856
Schubert-lu <lucas.gomes_2012@hotmail.com>
2022-04-17 21:37:56 -0300

    Primeiro Commit - Adicionando arquivos para o GitHub

---
+++ js/owl/setup.js
@@ -0,0 +1,20 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:2
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        },
+        2000:{
+            items:7
+        }
+
+    }
+})
